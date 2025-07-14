2da7e74558eb571e48f619dd5a85754d68f61af8
manoelaferpereira <129216680+manoelaferpereira@users.noreply.github.com>
2025-07-14 14:47:51 -0300

    Add files via upload
    Finish vogais

---
+++ VogaisC.c
@@ -0,0 +1,26 @@
+#include<stdio.h> 
+#include<ctype.h> // para tolower
+
+int main (void){
+    char [256];
+
+    printf("Digite uma frase : ");
+    fgets(s,sizeof s, stdin); //Lê as linhas completas,inclusive espaços.
+
+    int vogais = 0;
+    
+    scanf("%255s",s);
+
+    for(int i = 0; s=[i]; ++i){
+       switch (tolower()unsigned chars[i]) // compara as vogais de forma simples.
+    case 'a' :
+    case 'e': 
+    case 'i': 
+    case 'o': 
+    case'u':
+    ++vogais;
+    }
+}
+    printf("número de vogais : %d\n",vogais);
+    return 0;
+
